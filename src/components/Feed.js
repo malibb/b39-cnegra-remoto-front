430d0852b52853865191ed1f287e35a5a2b9eac1
Malinali <mali@devf.mx>
2020-04-16 21:44:20 -0500

    example to project Tender

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import { useQuery } from 'react-apollo-hooks';
 import gql from 'graphql-tag';
 import PostPreview from './PostPreview';
@@ -34,7 +34,19 @@ const ME=gql`
 `;
 
 function Feed({profile}){
+    const [actualPost, setActualPost] = useState();
+    const [index, setIndex] = useState(0);
     const { data, loading, error } = useQuery( profile ? ME : ALL_POST);
+    useEffect(() => {
+        if (data && index === 0){
+           setActualPost(data.getPosts[index]);
+           console.log('useEffect', data.getPosts[0], index, actualPost);
+        }
+        if(data && index <= data.getPosts.length) {
+            setActualPost(data.getPosts[index]);
+        }
+    }, [actualPost, data, index]);
+
     return(      
         <main className="container">
             {
@@ -43,23 +55,27 @@ function Feed({profile}){
                 : (error 
                 ? <h1>Hubo un error {error}</h1>
                     : profile ? 
-                    data.me.posts.map((post) => (
+                    data.me.posts.map((post) => ( 
                         <PostPreview 
-                        key={post._id} 
+                        key={actualPost} 
                         _id={post._id} 
                         title={post.title} 
                         author={post.author}
                         edit
                         remove/>
                     ))
-                    : data.getPosts.map((post) => (
-                        <PostPreview 
-                        key={post._id} 
-                        _id={post._id} 
-                        title={post.title} 
-                        author={post.author}
-                        date={post.createdAt} />
-                    ))
+                    : /* data.getPosts.map((post) => ( */
+                       actualPost ? 
+                       <><PostPreview 
+                        key={actualPost._id} 
+                        _id={actualPost._id} 
+                        title={actualPost.title} 
+                        author={actualPost.author}
+                        date={actualPost.createdAt} />
+                        <button onClick={()=>{setIndex(index+1)}}>Next</button>
+                        </>
+                        :<>No hay post actual</>
+                    /*))*/
                 )
             }    
         </main>
